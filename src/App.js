b64c1283e3339406c01e87a514b99e68dc52d6b2
efe <fabian.urbina@usach.cl>
2022-09-07 22:05:17 -0400

    Added main nav functionality

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function App() {
   const { t, i18n } = useTranslation();
+  const [headerOpen, setHeaderOpen] = useState(false);
+
+  const handleHeaderToggle = () => {
+    console.log("hey")
+    setHeaderOpen(headerOpen => !headerOpen);
+  }
 
   return (
     <div className="App">
-      <header>
+      <header className={`flex justify-end align-center ${headerOpen ? "open" : ""}`}>
         <div className="logo flex">
           efe
         </div>
-        <nav>
-          <ul>
-            <li>{t("about")}</li>
-            <li>{t("experience")}</li>
-            <li>{t("contact")}</li>
+        <nav className="hide-for-mobile">
+          <ul className="flex">
+            <li><a href="#">{`1. ${t("about")}`}</a></li>
+            <li><a href="#">{`2. ${t("experience")}`}</a></li>
+            <li><a href="#">{`3. ${t("contact")}`}</a></li>
+            <button>{t("resume")}</button>
+            <select
+              value={i18n.language}
+              onChange={(e) =>
+                i18n.changeLanguage(e.target.value)
+              }
+            >
+              <option value="en">English</option>
+              <option value="es">Español</option>
+            </select>
           </ul>
         </nav>
-        <select
-          value={i18n.language}
-          onChange={(e) =>
-            i18n.changeLanguage(e.target.value)
-          }
-        >
-          <option value="en">English</option>
-          <option value="es">Español</option>
-        </select>
+        <div className="toggle hide-for-desktop" onClick={handleHeaderToggle}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </header>
+      <div className={`overlay ${headerOpen ? "fade-in" : "fade-out"}`}>
 
+      </div>
     </div>
   );
 }
