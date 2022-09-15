14dd2574bb5c6bcae8e0147243fb953765b8aa95
efe <fabian.urbina@usach.cl>
2022-09-15 09:15:06 -0300

    Finished first draft

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@
 import { useEffect, useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
 
+function NavLinks({ slowDown }) {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <li><a href="#about" className={slowDown ? "slow-down" : ""}>{t("about")}</a></li>
+      <li><a className={slowDown ? "slow-down" : ""} href="#experience">{t("experience")}</a></li>
+      <li><a className={slowDown ? "slow-down" : ""} href="#contact">{t("contact")}</a></li>
+      <li><a className="button" href="https://getonbrd-prod.s3.amazonaws.com/uploads/cv/ba25467ac2da640b3831edf51bb4703d/Fabian_Urbina_Curriculum.pdf?X-Amz-Expires=86400&amp;X-Amz-Date=20220915T121348Z&amp;X-Amz-Algorithm=AWS4-HMAC-SHA256&amp;X-Amz-Credential=AKIAJT5MYUSOEN4SITVA%2F20220915%2Fus-east-1%2Fs3%2Faws4_request&amp;X-Amz-SignedHeaders=host&amp;X-Amz-Signature=abb8413f9e6e85cd2c3e399405dee187b06a83e7dd2da19e1f00148931011a93" download>
+        {t("resume")}
+      </a></li>
+    </>
+  )
+}
+
 function App() {
   const { t, i18n } = useTranslation();
   const [headerOpen, setHeaderOpen] = useState(false);
@@ -40,15 +55,12 @@ function App() {
   return (
     <div className="App">
       <header className={`flex justify-end align-center ${headerOpen ? "open" : ""}`}>
-        <div className="logo flex">
+        <a className="logo flex" href="#hero">
           efe
-        </div>
+        </a>
         <nav className="hide-for-mobile">
           <ul className="flex align-center">
-            <li className="show-down"><a href="#">{t("about")}</a></li>
-            <li className="show-down"><a href="#">{t("experience")}</a></li>
-            <li className="show-down"><a href="#">{t("contact")}</a></li>
-            <li className="show-down"><button>{t("resume")}</button></li>
+            <NavLinks slowDown={true} />
             <li className="show-down"><select
               value={i18n.language}
               onChange={(e) =>
@@ -76,11 +88,8 @@ function App() {
               <option value="en">English</option>
               <option value="es">Español</option>
             </select>
+              <NavLinks />
             </li>
-            <li><a href="#">{t("about")}</a></li>
-            <li><a href="#">{t("experience")}</a></li>
-            <li><a href="#">{t("contact")}</a></li>
-            <li><button>{t("resume")}</button></li>
           </ul>
         </aside>
       </header>
@@ -88,13 +97,13 @@ function App() {
       </div>
 
       <main className={headerOpen ? "blur" : ""}>
-        <section className="hero flex flex-column justify-center align-start">
+        <section id="hero" className="hero flex flex-column justify-center align-start">
           <h1 className="show-up">{t("salute")}</h1>
           <h2 className="show-up">Fabián Urbina.</h2>
           <h3 className="show-up">{t("shortDescription")}</h3>
           <p className="show-up">{t("description")}</p>
         </section>
-        <section className="about">
+        <section id="about" className="about">
           <div className="bio">
             <h4>{t("aboutMe")}</h4>
             <p>{t("bioFirst")}</p>
@@ -112,10 +121,22 @@ function App() {
           </div>
 
         </section>
-        <section className="experience">
+        <section id="experience" className="experience">
           <h4>{t("experience")}</h4>
+          <p>{t("experienceFirst")}</p>
+          <p>{t("experienceSecond")}</p>
+        </section>
+        <section id="contact" className="contact flex flex-column align-center">
+          <h4>{t("contact")}</h4>
+          <h2>{t("contactSubtitle")}</h2>
+          <p>{t("contactDescription")}</p>
+          <ul>
+            <li><a href="https://www.linkedin.com/in/fabian-urbina-ampuero-367349149/">LinkedIn</a></li>
+            <li><a href="mailto: fabian.urbina@usach.cl">Send Email</a></li>
+          </ul>
         </section>
       </main>
+      <footer></footer>
     </div>
   );
 }
